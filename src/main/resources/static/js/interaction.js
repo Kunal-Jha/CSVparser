93c357c4252e66615771d9c58cff1e510882a462
Kunal-Jha <kunal94jha@gmail.com>
2018-05-22 03:25:03 +0200

    added UI

--- src/main/resources/static/js/interaction.js
+++ src/main/resources/static/js/interaction.js
@@ -35,13 +35,56 @@ function getResult() {
 function printResults(data) {
 	$("#resultsection").show();
 	results = data;
-	var content = +"<table class=\"table\">  <thead> <tr> <th scope=\"col\">#</th><th scope=\"col\">Query</th><th scope=\"col\">File</th><th scope=\"col\">Result</th> <th scope=\"col\">TimeStamp</th></tr></thead>"
 
+	var content = "<table class=\"table\"> " + "<thead> " + "<tr> "
+			+ "<th scope=\"col\">#</th>" + "<th scope=\"col\">Query</th>"
+			+ "<th scope=\"col\">File</th>" + "<th scope=\"col\">Result</th>"
+			+ "</tr></thead> <tbody>"
 	content += "<tr> <th scope=\"row\">" + counter + "</th> " + "<td>"
-			+ queryFinal + "</td><td>" + documentName + "</td> <td>"
-			+ JSON.stringify(results) + "</td> <td>"
-			+ JSON.stringify(results.timestamp) + "</td></tr>"
+			+ queryFinal + "</td> " + "<td>" + documentName + "</td> " + "<td>"
+			+ JSON.stringify(results) + "</td>" + "</tr>"
+
+	content += "</tbody></table>"
 	counter += 1;
 
 	$("#resultTable").append($(content));
+}
+
+function buildHtmlTable(data) {
+	$("#resultsection").show();
+	results = data;
+	var col = [];
+	for (var i = 0; i < results.length; i++) {
+		for ( var key in results[i]) {
+			if (col.indexOf(key) === -1) {
+				col.push(key);
+			}
+		}
+	}
+
+	var table = document.createElement("table");
+
+	var tr = table.insertRow(-1); // TABLE ROW.
+
+	for (var i = 0; i < col.length; i++) {
+		var th = document.createElement("th"); // TABLE HEADER.
+		th.innerHTML = col[i];
+		tr.appendChild(th);
+	}
+
+	// ADD JSON DATA TO THE TABLE AS ROWS.
+	for (var i = 0; i < results.length; i++) {
+
+		tr = table.insertRow(-1);
+
+		for (var j = 0; j < col.length; j++) {
+			var tabCell = tr.insertCell(-1);
+			tabCell.innerHTML = myBooks[i][col[j]];
+		}
+	}
+
+	// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+	var divContainer = document.getElementById("resultTable");
+	divContainer.innerHTML = "";
+	divContainer.appendChild(table);
 }
